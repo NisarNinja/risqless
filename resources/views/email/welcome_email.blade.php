4eacd8e564bbe7c786f44499cdc031c4e1070fd9
jawad-gem <jawadahmed1024@gmail.com>
2023-01-20 19:29:49 +0500

    welcome email content change

--- resources/views/email/welcome_email.blade.php
+++ resources/views/email/welcome_email.blade.php
@@ -1,6 +1,6 @@
 
-Congratulations on your free trial with {{ config('app.name') }}!<br><br>
-
+{{-- Congratulations on your free trial with {{ config('app.name') }}!<br><br> --}}
+Congratulations, you have successfully signed up with us !
 If you haven’t done so already, click the link below to access your account and upload your first statement. <br><br>
 
 <a href="https://www.risqless.ai/login" class="btn btn-dark">Login</a> <br><br>
